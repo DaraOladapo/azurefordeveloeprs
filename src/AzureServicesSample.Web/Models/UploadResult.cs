ea43923983d418fc2238080c580d13aac7b96f47
Dara Oladapo <daraoladapo@outlook.com>
2020-06-17 16:47:10 +0100

    Sampy

---
+++ src/AzureServicesSample.Web/Models/UploadResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureServicesSample.Web.Models
+{
+    public class UploadResult
+    {
+        public string URL { get; set; }
+    }
+}
